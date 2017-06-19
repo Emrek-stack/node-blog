69f61ab6293875d649aa3e18f359c40ced08ab92
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-06-19 09:24:47 +0300

    ok

--- controllers/admin/blogController.js
+++ controllers/admin/blogController.js
@@ -1,11 +1,14 @@
+
+
 module.exports = {
     create: (req, res) => {
         res.render('web/home/index', {
             title: 'aa'
         });
     },
-    // create: (req, res) => {
+    create: (req, res) => {
 
-    // }
+
+    }
 
 }
